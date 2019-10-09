bf198cc0d18995df5418e4587ff207ff9190f374
odoo-pda <pda@odoo.com>
2019-10-09 10:59:25 +0200

    [INIT] add models

---
+++ recipe/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "recipe",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    # 'author': "pda",
+    # 'website': "https://odoo.com",
+
+    'category': 'Uncategorized',
+    'version': '0.0.1',
+
+    'depends': [
+    
+    ],
+
+    'data': [
+        'views/recipe.xml'
+
+    ],
+
+    'installable':True,
+}
---
+++ recipe/models/recipe.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from odoo.exceptions import Warning
+
+class RecipeBook(models.Model):
+    _name = 'recipe.book'
+
+    name = fields.Char()
+    recipe_type = fields.Selection([('starter','Starter'),('course','Course'),('dessert','Dessert')])
+
+    ingredient_table_ids = fields.One2many('recipe.ingredient.table', 'recipe_id')
+    season_ids = fields.Many2many('recipe.season')
+
+
+
+class RecipeIngredientTable(models.Model):
+    _name = 'recipe.ingredient.table'
+
+    recipe_id = fields.Many2one('recipe.book')
+    ingredient_id = fields.Many2one('recipe.ingredient')
+    quantity = fields.Float()
+    uom = fields.Selection([('kg','Kg'),('pc','Pc')])    
+
+
+class RecipeIngredient(models.Model):
+    _name = 'recipe.ingredient'
+
+    name = fields.Char()
+
+    ingredient_table_ids = fields.One2many('recipe.ingredient.table', 'ingredient_id')
+
+
+class RecipeSeason(models.Model):
+    _name = 'recipe.season'
+
+    name = fields.Char()
